8ed5d6dd1fe703ee7b8b6304a299673b37f44194
ToddCruts <tcruts@swbell.net>
2022-12-01 18:39:52 -0600

    Initial Project Setup
    Starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { CollectionComponent } from './collection/collection.component';
+import { SwapComponent } from './swap/swap.component';
+import { SearchComponent } from './search/search.component';
+import { ModelComponent } from './model/model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AuthenticationComponent,
+    CollectionComponent,
+    SwapComponent,
+    SearchComponent,
+    ModelComponent
   ],
   imports: [
     BrowserModule,
