10f7f1403f937d1007a0219560961ba1ae438f37
Heather Reynolds <vassindi@gmail.com>
2022-12-08 21:23:08 -0600

    "nav&routhing"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollectionComponent } from './collection/collection.component';
+import { SearchComponent } from './search/search.component';
+import { SwapComponent } from './swap/swap.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ // { path: "", redirectTo: "/collection", pathMatch: "full" },
+  { path: 'collection', component: CollectionComponent },
+  { path: 'swap', component: SwapComponent },
+  { path: 'search', component: SearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
